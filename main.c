dc799430a072b78755652e8734e8c05cbd08079a
Bellka58 <bellka58@gmail.com>
2017-11-19 22:36:45 +0300

    without if

--- main.c
+++ main.c
@@ -2,13 +2,16 @@
 
 int fled(int x)
 { 
-     PORTE = 1 << x
-}
+     int i, number;
+     for ( i = 0; i < x; i++){
+     number= 7 - i;
+     PORTE = 1 << number;
+}}
      
 
 int main()
 {
-  int n = 0;
+  int n = 1;
   int flag1=0,flag2=0;
   TRISE = 0;
   TRISD = 1 << 5;
@@ -18,45 +21,11 @@ int main()
     flag1 = flag2;
     if(PORTD & (1<<5))
      {
-      a = 1;
-      switch (n) {
-      case 0:
-          fled(7);
-          break;
-      case 1:
-          fled(7);
-          fled(6);
-          break;
-      case 2:
-          fled(7);
-          fled(6);
-          fled(5);
-          break;
-      case 3:
-          fled(7);
-          fled(6);
-          fled(5);
-          fled(4);
-          break;
-      case 4:
-          fled(7);
-          fled(6);
-          fled(5);
-          fled(4);
-          fled(3);
-          break;
-      case 5:
-          fled(7);
-          fled(6);
-          fled(5);
-          fled(4);
-          fled(3);
-          fled(2);
-          n = -1;
-          break;
-      default:
-          flag2 = 0;
+      flag2 = 1;
+      fled(n);
+      if (n == 6) n = 1;
       }
+     else flag2 = 0;
     if ((flag1 == 1) && (flag2 == 0)) 
       n = n + 1;
    }
